258357ed9a45f75e3fcdd903d26abc3b2659acd5
Peter Carswell <pcarswel@cscc.edu>
2016-03-01 15:50:33 -0500

    Initial commit

---
+++ src/com/pete/week04/Main.java
@@ -0,0 +1,25 @@
+package com.pete.week04;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static String readCity() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    //public static citiesList<String, int []>
+
+    public static void main(String[] args) {
+        String strCity = new String("");
+
+        strCity = readCity();
+
+        while (strCity != "end") {
+            
+        }
+    }
+}
